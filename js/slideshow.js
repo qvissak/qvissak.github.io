de65d1ae44d75a034bac58e5f4630c2e42f50453
Quinn Vissak <VissakQ@nxe.nxegen.com>
2017-07-18 23:22:30 -0700

    Add finesse to homepage and update about page
    Tighten CSS formatting on home page and update about me page with more
    relevant, interesting information

--- js/slideshow.js
+++ js/slideshow.js
@@ -3,7 +3,7 @@ carousel();
 
 function carousel() {
     var i;
-    var x = document.getElementsByClassName("mySlides");
+    var x = document.getElementsByClassName("slideshow");
     for (i = 0; i < x.length; i++) {
        x[i].style.display = "none";
     }
