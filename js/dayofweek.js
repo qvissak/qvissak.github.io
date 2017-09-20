b19af572e8928d1b1f44b29dbfbcc9541002efc6
qmv13001 <quinn.vissak@uconn.edu>
2017-09-20 18:15:42 -0400

    Update welcome page description
    Insert Ray Greenly Scholar information on welcome page

---
+++ js/dayofweek.js
@@ -0,0 +1,15 @@
+getDayString();
+
+function getDayString() {
+    var d = new Date();
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+    document.getElementById("dotw").innerHTML = weekday[d.getDay()];
+}
